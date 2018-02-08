84a1737ceece2544b18a57bfe14e38e7c6ef9ce5
Steve <levkoff@gmail.com>
2018-02-07 16:51:09 -0800

    code from 2-7-18 class on installing swirl

---
+++ code 2-7-18.R
@@ -0,0 +1,14 @@
+##notes on syntax and commands from 2/7/18 lecture
+
+##comments look like this  
+
+
+install.packages("swirl")
+
+library("swirl")
+
+swirl()
+
+library(swirl)
+install_course_github("swirldev", "R_Programming_E")
+swirl()
